9dbed53ed4cdac1ba203922b03919fe1d9406af5
jatin <jatinkalia08@gmail.com>
2025-04-25 00:14:55 +0530

    add functionality to the signup page

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //adding proxy because our frontend and be on different ports and when we fetch '/api/auth/signup' we need to add proxy i.e whenever we are trying to fetch something starting with '/api' on FE we simply add proxy for server
+  server:{
+  proxy:{
+    '/api':{
+      target:'http://localhost:3000',
+      secure:false,
+    }
+  }
+  },
   plugins: [react()]
 })
