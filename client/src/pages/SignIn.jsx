f9368e48b8829cb578c69ef7eca9794e88431425
jatin <jatinkalia08@gmail.com>
2025-04-30 11:31:32 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,11 +2,14 @@ import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React from 'react'
 import { useState } from 'react'
 import { Link ,useNavigate} from 'react-router-dom'
+import { useDispatch,useSelector } from 'react-redux'
+import { signInStart,signinSuccess,signInFailure } from '../redux/user/userSlice';
+
 
 const SignIn = () => {
   const [formData,setFormData]=useState({});
-  const [errorMessage,setErrorMessage]=useState(null);
-  const [loading,setLoading]=useState(false);
+  const {loading,error:errorMessage}= useSelector(state => state.user);
+  const dispatch=useDispatch();
   const navigate=useNavigate();
 
   const handleChange=(e)=>{
@@ -17,28 +20,26 @@ const SignIn = () => {
   e.preventDefault();
 //error
   if(!formData.email || !formData.password){
-    return setErrorMessage('Please fill out all the fields');
+    return dispatch(signInFailure('Please fill out all the fields'));
   }
   try {
-    setLoading(true);
-    setErrorMessage(null);
+    dispatch(signInStart());
     const res=await fetch('/api/auth/signin',{
       method:'POST',
       headers:{'Content-Type':'application/json'},
       body:JSON.stringify(formData),
     })
     const data= await res.json();
-    //same username then error i.e if user already exists and we try signup wuth same username then error
+    //same username then error i.e if user already exists and we try signup with same username then error
     if (data.success === false){
-      return setErrorMessage(data.message);
+      dispatch(signInFailure(data.message));
     }
-    setLoading(false);
     if(res.ok){
+      dispatch(signinSuccess(data));
       navigate('/');
     }
   } catch (error) {
-    setErrorMessage(error.message);//error on client side 
-    setLoading(false);
+    dispatch(signInFailur(error.message));//error on client side 
   }
   }
 
