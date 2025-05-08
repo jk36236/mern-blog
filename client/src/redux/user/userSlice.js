b0965fd656e3932fb2c8a91be9c9fb8791d50f60
jatin <jatinkalia08@gmail.com>
2025-05-08 12:05:33 +0530

    complete delete user account functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -39,9 +39,21 @@ const userSlice=createSlice({
       state.loading=false;
       state.error=action.payload;
     },
+    deleteUserStart:(state)=>{
+      state.loading= false;
+      state.error=null;
+    },
+    deleteUserSuccess:(state)=>{
+      state.currentUser=null;
+      state.loading=false;
+      state.error=null;
+    },deleteUserFailure:(state,action)=>{
+      state.loading=false;
+      state.error=action.payload;
+    }
   }
 });
 
-export const {signInStart, signInSuccess, signInFailure,updateStart,updateSuccess,updateFailure} = userSlice.actions;
+export const {signInStart, signInSuccess, signInFailure,updateStart,updateSuccess,updateFailure,deleteUserStart,deleteUserFailure,deleteUserSuccess} = userSlice.actions;
 
 export default userSlice.reducer;// defulat export therefore can rename it while importing in other module
