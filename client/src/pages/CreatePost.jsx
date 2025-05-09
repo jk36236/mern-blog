4f9fb95b24c0f1f10d9a439b3218a91e853324ac
jatin <jatinkalia08@gmail.com>
2025-05-09 10:58:19 +0530

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -2,12 +2,15 @@ import {Alert, Button, FileInput, Select, TextInput} from 'flowbite-react';
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useNavigate } from 'react-router-dom';
 
 const CreatePost = () => {
   const [file,setFile]=useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
-
+  const [publishError,setPublishError]=useState(null);
+  const navigate=useNavigate();
+// console.log(formData);
   const handleUploadImage=async()=>{
      try {
       if(!file){
@@ -30,8 +33,8 @@ const CreatePost = () => {
   
   .then(resp => resp.json())
   .then(data => {
-    console.log(data);
-    console.log(data.url);
+    // console.log(data);
+    // console.log(data.url);
     setImageUploadError(null);
     setFormData({ ...formData, image: data.url}); 
   })
@@ -41,14 +44,39 @@ const CreatePost = () => {
     }
   }
 
+const handleSubmit=async (e)=>{
+  e.preventDefault();
+  try {
+    const res= await fetch('/api/post/create',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    });
+    const data=await res.json();
+    if(!res.ok){
+      setPublishError(data.message);
+      return;
+    }
+    
+    if(res.ok){
+      setPublishError(null);
+      navigate(`/post/${data.slug}`);//we'll navigate each post to different page using slug
+    }
+  } catch (error) {
+    setPublishError('Something went wrong');
+  }
+}
+
 
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-         <TextInput type='text' placeholder='Title' required id='title' className='flex-1'/>
-        <Select>
+         <TextInput type='text' placeholder='Title' required id='title' className='flex-1' onChange={(e)=>setFormData({...formData,title:e.target.value})}/>
+        <Select onChange={(e)=> setFormData({...formData,category:e.target.value})}>
            <option value="uncategorized">Select a category</option>
            <option value="javascript">Javascript</option>
            <option value="reactjs">React.js</option>
@@ -58,7 +86,7 @@ const CreatePost = () => {
       
       <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
         <FileInput type='file' accept='image/*' onChange={(e)=>setFile(e.target.files[0])}/>
-        <Button type='button' gradientDuoTone='purpleToBlue' size="sm" outline onClick={handleUploadImage}>Upload Image</Button>
+        <Button type='button' gradientDuoTone='purpleToBlue' size="sm" outline onClick={handleUploadImage} >Upload Image</Button>
       </div>
       {imageUploadError &&  <Alert color='failure'>{imageUploadError}</Alert>}
 
@@ -70,10 +98,17 @@ const CreatePost = () => {
           />
       )}
      
-       <ReactQuill theme='snow' placeholder='Write something...' className='h-72 mb-12'  required/>
-      </form>
+       <ReactQuill theme='snow' placeholder='Write something...' className='h-72 mb-12'  required
+        onChange={(value)=>{setFormData({...formData,content:value})}}
+       />
+      
 
       <Button type='submit' gradientDuoTone='purpleToPink'>Publish </Button>
+
+      {
+        publishError && <Alert className='mt-5' color='failure'>{publishError}</Alert>
+      }
+      </form>
     </div>
   )
 }
